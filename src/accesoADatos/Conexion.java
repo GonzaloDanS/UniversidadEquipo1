e64004b02d4257ea141ac289f540e5a01ade0db9
GonzaloDanS <gonzalodansosa@gmail.com>
2023-09-08 16:20:16 -0300

    Funcion getConnection
    Creada la funcion getConnection en la clase conexion

--- src/accesoADatos/Conexion.java
+++ src/accesoADatos/Conexion.java
@@ -1,7 +1,32 @@
 package accesoADatos;
 
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
 public class Conexion {
-    private final String URL="";
-    private final String DB="";
+    private static final String URL="jdbc:mariadb://localhost:";
+    private static final String PUERTO="3306/";
+    private static final String DB="universidadGrupo1";
+    private static final String USUARIO="root";
+    private static final String PASSWORD="";
+    private static Connection con;
     
+    private Conexion(){}
+    
+    public static Connection getConnection(){
+        if (con==null) {
+            try {
+                Class.forName("org.mariadb.jdbc.Driver");
+                con = DriverManager.getConnection(URL+PUERTO+DB, USUARIO, PASSWORD);
+                
+            } catch (ClassNotFoundException ex) {
+                JOptionPane.showMessageDialog(null, "Error al cargar los drivers - "+ex.getLocalizedMessage());
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(null, "Error al conectar con la base de datos - "+ex.getLocalizedMessage());
+            }
+        }
+        return con;
+    }
 }
