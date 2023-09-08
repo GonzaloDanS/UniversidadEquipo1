72e8e8688e1f0f2ea79380ccb38fb47f55ce960f
Paau93 <paau_rp.93@hotmail.com>
2023-09-08 17:17:49 -0300

    Update Alumno.java
    Agregado el dni

--- src/entidades/Alumno.java
+++ src/entidades/Alumno.java
@@ -3,13 +3,16 @@ package entidades;
 import java.time.LocalDate;
 
 public class Alumno {
+
     private int idAlumno;
-    private String apellido,nombre;
+    private int dni;
+    private String apellido, nombre;
     private LocalDate fechaNac;
     private boolean activo;
 
-    public Alumno(int idAlumno, String apellido, String nombre, LocalDate fechaNac, boolean activo) {
+    public Alumno(int idAlumno, int dni, String apellido, String nombre, LocalDate fechaNac, boolean activo) {
         this.idAlumno = idAlumno;
+        this.dni = dni;
         this.apellido = apellido;
         this.nombre = nombre;
         this.fechaNac = fechaNac;
@@ -19,7 +22,8 @@ public class Alumno {
     public Alumno() {
     }
 
-    public Alumno(String apellido, String nombre, LocalDate fechaNac, boolean activo) {
+    public Alumno(int dni, String apellido, String nombre, LocalDate fechaNac, boolean activo) {
+        this.dni = dni;
         this.apellido = apellido;
         this.nombre = nombre;
         this.fechaNac = fechaNac;
@@ -32,6 +36,14 @@ public class Alumno {
 
     public void setIdAlumno(int idAlumno) {
         this.idAlumno = idAlumno;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
     }
 
     public String getApellido() {
@@ -68,9 +80,7 @@ public class Alumno {
 
     @Override
     public String toString() {
-        return "Alumno{" + "idAlumno=" + idAlumno + ", apellido=" + apellido + ", nombre=" + nombre + ", fechaNac=" + fechaNac + ", activo=" + activo + '}';
+        return "Alumno{" + "idAlumno=" + idAlumno + "Dni=" + dni + ", apellido=" + apellido + ", nombre=" + nombre + ", fechaNac=" + fechaNac + ", activo=" + activo + '}';
     }
 
-    
-    
 }
