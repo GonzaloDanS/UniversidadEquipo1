8bdef3123aa0fb84e19d211198acc7b7ec4014d5
Paau93 <paau_rp.93@hotmail.com>
2023-09-08 17:16:56 -0300

    Create AlumnoData.java

---
+++ src/accesoADatos/AlumnoData.java
@@ -0,0 +1,45 @@
+package accesoADatos;
+
+import entidades.Alumno;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+public class AlumnoData {
+    private Connection con = null;
+    
+    public AlumnoData () {
+        con=Conexion.getConnection();
+    }
+    
+    public void guardarAlumno (Alumno alumno){
+        String sql = "INSERT INTO alumno (dni, apellido, nombre, fechaNacimiento,estado) "
+                + "VALUES (?,?,?,?,?)";
+        try {
+            PreparedStatement ps = con.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS);
+            ps.setInt(1, alumno.getDni());
+            ps.setString(2, alumno.getApellido());
+            ps.setString(3, alumno.getNombre());
+            ps.setDate(4, Date.valueOf(alumno.getFechaNac()));
+            ps.setBoolean(5, alumno.isActivo());
+            int mostrar = ps.executeUpdate();
+            ResultSet rs = ps.getGeneratedKeys();
+            
+            if (rs.next()){
+                alumno.setIdAlumno(rs.getInt("idAlumno"));
+                
+                JOptionPane.showMessageDialog(null, "Alumno añadido");
+            }
+            ps.close();
+            
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al conectar con la tabla alumno");
+        }
+    }
+}
